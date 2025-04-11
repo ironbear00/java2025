ae4f40afbd2ef4dd93b014fd9ffe8cf1371a901c
ironbear <kango100423@gmail.com>
2025-04-11 14:37:39 +0900

    추가: test files commit

--- test_java_proj/src/ch05_ex/Stack.java
+++
@@ -1,9 +0,0 @@
-package ch05_ex;
-
-interface Stack
-{
-	int length();
-	int capacity();
-	String pop();
-	boolean push(String val);
-}
--- test_java_proj/src/ch05_ex/TV.java
+++
@@ -1,9 +0,0 @@
-package ch05_ex;
-
-class TV
-{
-	private int size;
-	
-	public TV(int size) {this.size =size;}
-	protected int getSize() {return size;}
-}
